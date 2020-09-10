0e53dca40c2266fdd4a0e20756351af964bfd903
Christopher Spraggett <cspraggett@gmail.com>
2020-09-09 23:46:52 -0400

    Start writing othello helpers

--- src/games/othello/helpers.js
+++ src/games/othello/helpers.js
@@ -1,6 +1,6 @@
 const { Map, List, toJS } = require("immutable");
 
-const otherStone = Map({ B: "W", W: "B" });
+const otherSide = Map({ B: "W", W: "B" });
 
 const initialBoard = List()
   .setSize(64)
@@ -9,17 +9,39 @@ const initialBoard = List()
   .set(28, "B")
   .set(35, "B")
   .set(36, "W");
-console.log(initialBoard.toJS());
-const adjacentSquares = Map({
-  up: (x) => (x > 7 ? x - 8 : null),
-  down: (x) => (x < 56 ? x + 8 : null),
-  left: (x) => (x % 8 !== 0 ? --x : null),
-  right: (x) => ((x + 1) % 8 !== 0 ? ++x : null),
-  upLeft: (x) => (x >= 8 || x % 8 !== 0 ? x - 9 : null),
-  upRight: (x) => (x >= 8 || ++x % 8 !== 0 ? x - 7 : null),
-  downLeft: (x) => (x <= 55 || x % 8 !== 0 ? x + 7 : null),
-  downRight: (x) => (x <= 55 || ++x % 8 !== 0 ? x + 9 : null),
-});
 
-const x = adjacentSquares.get("left");
-console.log(x(9));
+const adjacentSquares = List([
+  function up(x) {
+    return x > 7 ? x - 8 : null;
+  },
+  function down(x) {
+    return x < 56 ? x + 8 : null;
+  },
+  function left(x) {
+    return x % 8 !== 0 ? --x : null;
+  },
+  function right(x) {
+    return (x + 1) % 8 !== 0 ? ++x : null;
+  },
+  function upLeft(x) {
+    return x >= 8 && x % 8 !== 0 ? x - 9 : null;
+  },
+  function upRight(x) {
+    return x >= 8 && (x + 1) % 8 !== 0 ? x - 7 : null;
+  },
+  function downLeft(x) {
+    return x <= 55 && x % 8 !== 0 ? x + 7 : null;
+  },
+  function downRight(x) {
+    return x <= 55 && (x + 1) % 8 !== 0 ? x + 9 : null;
+  },
+]);
+
+const neighbouringSquares = (move) => {
+  return adjacentSquares.map((curr) => {
+    console.log("curr: ", curr, "move: ", curr(move));
+    return List([curr, curr(move)]);
+  });
+};
+
+module.exports = { otherSide, initialBoard, adjacentSquares };
